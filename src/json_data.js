74568ef08fbb9801c5565d53724b8669331f5d20
Kevin Bost <kevbost@gmail.com>
2016-03-30 10:27:29 -0400

    initial commit

---
+++ src/json_data.js
@@ -0,0 +1,13 @@
+import $ from 'jquery';
+
+export class json_data {
+    constructor(){
+        this.name = 'Kevin';
+        this.hello = 'to the Aurelia Navigation App!';
+        this.greeting = `Welcome, ${this.name}, ${this.hello}`;
+    }
+
+    activate() {
+        // $('.page-host').css('pointer-events', 'none')
+    }
+}
